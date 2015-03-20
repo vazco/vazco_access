b70268ce32128f8c3d9770c52a3d6a9f59fee520
cristo-rabani <cristo.rabani@gmail.com>
2015-03-20 13:33:01 +0100

    .

--- package.js
+++ package.js
@@ -14,23 +14,3 @@ Package.on_use(function(api) {
     api.add_files('access.js', ['client', 'server']);
     api.add_files('publication.js', ['server']);
 });
-
-Package.on_test(function(api) {
-    api.use([
-        'coffeescript',
-        'vazco:tools-common',
-        'vazco:access',
-        'underscore',
-        'accounts-base',
-        'tinytest',
-        'test-helpers'
-    ], ['client', 'server']);
-
-    api.add_files([
-        'access_test.js'
-    ], ['client', 'server']);
-
-    api.add_files([
-        'publication_tests.coffee'
-    ], ['server']);
-});
