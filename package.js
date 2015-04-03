e8eb22bd8325b3505480adbbffe4e4a7174486e5
cristo-rabani <cristo.rabani@gmail.com>
2015-04-03 15:02:58 +0200

    up

--- package.js
+++ package.js
@@ -3,13 +3,13 @@
 Package.describe({
     summary: 'Vazco Access',
     name: 'vazco:access',
-    version: "1.0.5",
+    version: "1.1.0",
     git: "https://github.com/vazco/vazco_access.git"
 });
 
 Package.on_use(function(api) {
-    api.versionsFrom('METEOR@0.9.3');
-    api.use(['vazco:tools-common@1.0.0', 'underscore', 'accounts-base'], ['client', 'server']);
+    api.versionsFrom('METEOR@1.0.4');
+    api.use(['vazco:universe-core@1.6.5', 'underscore', 'accounts-base'], ['client', 'server']);
     api.imply('vazco:tools-common');
     api.add_files('access.js', ['client', 'server']);
     api.add_files('publication.js', ['server']);
